e5db44eb962316a36a4bffad2021577c8284713d
power721 <power721@163.com>
2018-04-27 21:38:45 +0800

    add SystemEnviromentConfiguration

---
+++ src/main/java/org/power/configuration/SystemEnviromentConfiguration.java
@@ -0,0 +1,57 @@
+package org.power.configuration;
+
+import java.io.IOException;
+
+public class SystemEnviromentConfiguration implements Configuration {
+
+    @Override
+    public String get(String key) {
+        return System.getenv(key);
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        String value = System.getenv(key);
+        return value == null ? defaultValue : value;
+    }
+
+    @Override
+    public String set(String key, String value) {
+        return null;
+    }
+
+    @Override
+    public String save(String key, String value) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Integer save(String key, Integer value) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Long save(String key, Long value) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Double save(String key, Double value) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Boolean save(String key, Boolean value) throws IOException {
+        return null;
+    }
+
+    @Override
+    public void reload() throws IOException {
+
+    }
+
+    @Override
+    public void save() throws IOException {
+
+    }
+}
